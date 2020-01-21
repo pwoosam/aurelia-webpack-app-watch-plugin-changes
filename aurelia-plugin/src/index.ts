11bc63dbe4d935501a13bc4716a96e9bb5a8e903
pwoosam <pwoosam@baytechconsulting.com>
2020-01-21 08:37:59 -0800

    Export plugin c;asses

--- aurelia-plugin/src/index.ts
+++ aurelia-plugin/src/index.ts
@@ -1,6 +1,8 @@
 import {FrameworkConfiguration} from 'aurelia-framework';
 import {PLATFORM} from 'aurelia-pal';
 
+export * from './elements/hello-world';
+
 export function configure(config: FrameworkConfiguration) {
   config.globalResources([
     PLATFORM.moduleName('./elements/hello-world')
